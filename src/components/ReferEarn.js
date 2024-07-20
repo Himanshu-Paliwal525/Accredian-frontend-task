08e3664f7133bdff51da0b582a4d2b5a17ba58a4
Himanshu Paliwal <paliwalhimanshu525@gmail.com>
2024-07-20 22:47:22 +0530

    url updated

--- src/components/ReferEarn.js
+++ src/components/ReferEarn.js
@@ -1,9 +1,8 @@
 // src/components/ReferEarnPage.js
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+
 const ReferEarnPage = () => {
     const [open, setOpen] = useState(false);
-    const navigate = useNavigate();
     const [formValues, setFormValues] = useState({
         referrerName: "",
         referrerEmail: "",
@@ -24,8 +23,7 @@ const ReferEarnPage = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(formValues);
-        const response = await fetch("http://localhost:4000/referrals", {
+        const response = await fetch("https://accredian-backend-task-0i3g.onrender.com/referrals", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -33,16 +31,14 @@ const ReferEarnPage = () => {
             body: JSON.stringify(formValues),
         });
         if (response) {
+            handleClose();
             alert("Form submitted");
-            navigate("/");
         }
-
-        handleClose();
     };
 
     return (
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            <div className="bg-red p-10 rounded-lg shadow-lg text-center">
+            <div className="bg-white p-10 rounded-lg shadow-lg text-center">
                 <h1 className="text-4xl font-bold mb-4">Refer & Earn</h1>
                 <p className="text-lg mb-6">
                     Refer your friends to our course and earn rewards!
@@ -56,13 +52,13 @@ const ReferEarnPage = () => {
             </div>
             {open && (
                 <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-                    <div className="bg-gradient-to-r from-slate-300 via-blue-200 to-slate-300 rounded-3xl shadow-lg w-1/3 px-11 py-11">
-                        <div className="flex justify-between items-center mb-4">
-                            <h2 className="text-2xl font-bold">
+                    <div className="bg-gradient-to-r from-blue-300 via-blue-200 to-blue-300 p-6 rounded-3xl shadow-lg w-1/3 px-11 py-9 font-sansPop">
+                        <div className="flex justify-between items-center mb-8">
+                            <h2 className="text-3xl font-bold">
                                 Referral Form
                             </h2>
                             <button
-                                className="text-gray-500 hover:text-gray-700"
+                                className=" text-gray-500 hover:text-gray-700"
                                 onClick={handleClose}
                             >
                                 Close
@@ -70,7 +66,7 @@ const ReferEarnPage = () => {
                         </div>
                         <form
                             onSubmit={handleSubmit}
-                            className="flex flex-col items-center justify-center"
+                            className="flex flex-col items-center"
                         >
                             <div className="mb-4 w-full">
                                 <label className="block text-left mb-1">
@@ -83,6 +79,7 @@ const ReferEarnPage = () => {
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 border-none outline-none rounded-xl"
                                     required
+                                    placeholder="Enter name here..."
                                 />
                             </div>
                             <div className="mb-4 w-full">
@@ -96,6 +93,7 @@ const ReferEarnPage = () => {
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 border-none outline-none rounded-xl"
                                     required
+                                    placeholder="Enter email here..."
                                 />
                             </div>
                             <div className="mb-4 w-full">
@@ -109,6 +107,7 @@ const ReferEarnPage = () => {
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 border-none outline-none rounded-xl"
                                     required
+                                    placeholder="Enter name here..."
                                 />
                             </div>
                             <div className="mb-4 w-full">
@@ -122,11 +121,12 @@ const ReferEarnPage = () => {
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 border-none outline-none rounded-xl"
                                     required
+                                    placeholder="Enter email here..."
                                 />
                             </div>
                             <button
                                 type="submit"
-                                className="bg-blue-700 text-white px-4 py-2 rounded-full hover:bg-blue-800 transition duration-300"
+                                className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300 "
                             >
                                 Submit
                             </button>
